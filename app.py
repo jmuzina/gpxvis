0be40835facb09fcb97da730652f881dea1934a7
J. Muzina <jmuzina@pm.me>
2022-01-28 17:04:01 -0500

    comments for clarity

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-# apt install python3.8-venv
-# sudo apt-get install python3-virtualenv
-# sudo apt-get install python3
-# python3 -m venv venv/
-# . venv/bin/activate
-# pip3 install flask
-# pip3 install Flask-Assets
-# pip3 install pyscss
-# pip3 install configparser
-# pip3 install requests
+# App launch-point
 
+# ---- Dependency imports ---- #
 from flask import Flask, request, render_template, session, redirect, url_for
 import json
 import urllib.request
 import urllib.parse
 from flask_assets import Environment, Bundle
 from configparser import RawConfigParser, ConfigParser
+# ---------------------------- #
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-
+# ---- Bundle all scss files into all.css ---- #
 assets     = Environment(app)
 assets.url = app.static_url_path
 scss       = Bundle('style.scss', filters='pyscss', output='all.css')
@@ -31,18 +24,21 @@ assets.config['PYSCSS_ASSETS_URL'] = assets.url
 assets.config['PYSCSS_ASSETS_ROOT'] = assets.directory
 
 assets.register('scss_all', scss)
-
+# -------- Read and load config data -------- #
 configParser = RawConfigParser()   
 configFilePath = r'./app.cfg'
 configParser.read(configFilePath)
 
 config = ConfigParser()
 config.readfp(open(r'./app.cfg'))
+# -------------------------------------------- #
 
-session = {}
+session = {} # Clear session on server reboot
 
+# Index page
 @app.route('/')
 def render_index():
+    # Render homepage with userdata if it exists
     try:
         return render_template("index.html", cfg=config, userData = session['userData'])
     except:
@@ -50,13 +46,15 @@ def render_index():
 
 @app.route('/logout')
 def logout():
-    session.pop('userData')
+    session.pop('userData') # Clear user session data
     return redirect(url_for('render_index'))
 
+# Load every supported login network and define routes for them based on cfg file
 for cfgSectionName in config:
     if cfgSectionName != "DEFAULT":
         @app.route('/'+cfgSectionName+'-login')
         def login():
+            # Assemble POST headers using auth code returned from API
             params = {    
                 "client_id": config[cfgSectionName]['CLIENT_ID'].strip('\''),    
                 "client_secret": config[cfgSectionName]['CLIENT_SECRET'].strip('\''),    
@@ -66,11 +64,13 @@ for cfgSectionName in config:
                 
             query_string = urllib.parse.urlencode( params )    
             data = query_string.encode( "ascii" )    
-                
+            
+            # Send user auth code, app credentials to API to request their details
             with urllib.request.urlopen( config[cfgSectionName]['TOKEN_URL'].strip('\''), data ) as response:     
                 response = json.loads(response.read())
-                session['userData'] = response['athlete']
+                session['userData'] = response['athlete'] # Store session data
                 return redirect(url_for('render_index'))
     else:
+        # Store any config items not related to API logins under app.config
         for x in config[cfgSectionName]:
             app.config[x] = config[cfgSectionName]
