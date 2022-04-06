8d9add2e1253328777463f87d7d4a5896bbb74d6
Joe Muzina <joe.muzina@gmail.com>
2022-04-06 15:21:28 -0400

    Image and GPX file verification for  extension and filesize

--- static/fileVerification.js
+++ static/fileVerification.js
@@ -5,19 +5,79 @@ const imageFormats = {
     "gif": true
 };
 
-const maxImageSize = 50; // megabytes
+const fileRestrictions = {
+    "backgroundImage":  {
+        "extensions": {
+            "png": true,
+            "jpg": true,
+            "jpeg": true,
+            "gif": true
+        },
+        "maxSize": 50 // MB
+    },
+    "uploadedActivity": {
+        "extensions": {
+            "gpx": true
+        },
+        "maxSize": 50
+    }
+}
 
-function verifyBackgroundImage() {
-    const uploadBtn = document.getElementById("backgroundImage");
-    const uploadedFile = uploadBtn.value;
-    const fileName = uploadedFile.toLowerCase();
-    const fileExtension = fileName.split(".").pop();
+function fileUploadIsValid(fileType) {
+    if (fileRestrictions[fileType] && fileRestrictions[fileType]["extensions"] && fileRestrictions[fileType]["maxSize"]) {
+        const uploadBtn = document.getElementById(fileType);
+        if (uploadBtn !== null) {
+            const uploadedFile = uploadBtn.value;
+            const fileName = uploadedFile.toLowerCase();
+            const fileExtension = fileName.split(".").pop();
 
-    if (imageFormats[fileExtension] == true) {
-        const fileSize = uploadBtn.files[0].size / 1000000; // megabytes
-        return (fileSize < maxImageSize);
+            if (fileRestrictions[fileType]["extensions"][fileExtension] == true) {
+                const fileSize = uploadBtn.files[0].size / 1000000; // MB
+                if (fileSize < fileRestrictions[fileType]["maxSize"]) {
+                    return {
+                        "success": true,
+                        "message": ""
+                    };
+                }
+                else {
+                    return {
+                        "success": false,
+                        "message": "Uploaded file must be smaller than " + fileRestrictions[fileType]["maxSize"].toString() + " MB."
+                    };
+                }
+            }
+            else {
+                return {
+                    "success": false,
+                    "message": "Uploaded file can only be "  + Object.keys(fileRestrictions[fileType]["extensions"]).toString() + "."
+                };
+            }
+        }
+        else {
+            return {
+                "success": false,
+                "message": "Could not find upload button."
+            };
+        }
     }
     else {
-        return false;
+        return {
+            "success": false,
+            "message": "No matching file restriction configuration for type " + fileType + "."
+        };
+    }
+}
+
+function verifyBackgroundImage(fileType = "") {
+    const fileVerificationResult = fileUploadIsValid(fileType);
+    const uploadBtn = document.getElementById(fileType);
+    if (fileVerificationResult["success"]) {
+        console.log("success");
+    }
+    else {
+        alert(fileVerificationResult["message"]);
+        if (uploadBtn !== null) {
+            uploadBtn.value = null;
+        }
     }
 }
