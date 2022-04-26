483a5b84a5c5d1ede622d9d2465e19b0921a1ad7
Joe Muzina <jmuzina@pm.me>
2022-04-26 07:23:34 -0400

    remove unnecessary line

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/root/customer-account-automation/")
 
 from app import flaskApp as application
 flaskApp = application
